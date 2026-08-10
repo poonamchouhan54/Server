f2a40c1b92b73df567aa83ce337c5ad0df77207a
poonamchouhan54 <71801966+poonamchouhan54@users.noreply.github.com>
2026-08-10 22:05:20 +0530

    Update jio.js

--- api/jio.js
+++ api/jio.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 export default async function handler(req, res) {
-    const jsonUrl = "https://wispy-wave-3131.diwij76343.workers.dev/"; 
+    const jsonUrl = "https://sonujson-v3.pages.dev/Data/sports.json"; 
 
     try {
         const response = await fetch(jsonUrl);
