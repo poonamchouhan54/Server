268c4084709dfa2a3a5f5a3147e137f0ca41b7f8
poonamchouhan54 <71801966+poonamchouhan54@users.noreply.github.com>
2026-09-04 07:15:26 +0530

    Update tv.js

--- api/tv.js
+++ api/tv.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     const url1 = "https://mainplaylist.poonamchouhan076.workers.dev/";
     const url3 = "https://wandering-morning-5534.poonamchouhan076.workers.dev/";
     const url4 = "https://upcominge-dawn-7c6bve.poonamchouhan076.workers.dev/";
-    const url5 = "https://fancode-art-c9de.poonamchouhan076.workers.dev/";
+    const url5 = "https://aged-fancode-0041.poonamchouhan076.workers.dev/";
     const url6 = "https://sonyliv-event-5e05.poonamchouhan076.workers.dev/";
 
     const fetchWithTimeout = async (url, ms = 5000) => {
