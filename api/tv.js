6c4878a9e5454bc61ee9776767bfeaa055c4d1a1
poonamchouhan54 <71801966+poonamchouhan54@users.noreply.github.com>
2026-09-10 16:09:47 +0530

    Update tv.js

--- api/tv.js
+++ api/tv.js
@@ -11,9 +11,10 @@ export default async function handler(req, res) {
     const url1 = "https://mainplaylist.poonamchouhan076.workers.dev/";
     const url3 = "https://raw.githubusercontent.com/Prtstream820894/Api/refs/heads/main/jtv_my.txt";
     const url4 = "https://upcominge-dawn-7c6bve.poonamchouhan076.workers.dev/";
-    const url5 = "https://sonyliv-event-5e05.poonamchouhan076.workers.dev/";
-    const url6 = "https://fancode8-star-4aa7.poonamchouhan076.workers.dev/";
-const url7 = "https://primcricl-mud-e2d1.poonamchouhan076.workers.dev/";
+const url5 = "https://star-sports-images.prtstream.workers.dev/";
+    const url6 = "https://sonyliv-event-5e05.poonamchouhan076.workers.dev/";
+    const url7 = "https://fancode8-star-4aa7.poonamchouhan076.workers.dev/";
+const url8 = "https://primcricl-mud-e2d1.poonamchouhan076.workers.dev/";
 
     const fetchWithTimeout = async (url, ms = 5000) => {
       if (!url) return "";
@@ -59,6 +60,7 @@ const url7 = "https://primcricl-mud-e2d1.poonamchouhan076.workers.dev/";
       fetchWithTimeout(url5, 60000),
       fetchWithTimeout(url6, 60000),
 fetchWithTimeout(url7, 30000),
+fetchWithTimeout(url8, 30000),
     ]);
 
     if (!responses[0]) {
