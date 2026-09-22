c26f157964a0c7053fc5635cb6720d731567a0cf
poonamchouhan54 <71801966+poonamchouhan54@users.noreply.github.com>
2026-09-22 09:35:54 +0530

    Update tv.js

--- api/tv.js
+++ api/tv.js
@@ -11,9 +11,9 @@ export default async function handler(req, res) {
     const url1 = "https://mainplaylist.poonamchouhan076.workers.dev/";
     const url3 = "https://raw.githubusercontent.com/Prtstream820894/Api/refs/heads/main/jtv_my.txt";
     const url4 = "https://upcominge-dawn-7c6bve.poonamchouhan076.workers.dev/";
-const url5 = "https://sony-live-new2026.poonamchouhan076.workers.dev/";
+const url5 = "https://billowing-fancode-1fec.poonamchouhan076.workers.dev/";
     const url6 = "https://star-sports-images.prtstream.workers.dev/";
-    const url7 = "https://billowing-fancode-1fec.poonamchouhan076.workers.dev/";
+    const url7 = "https://sony-live-new2026.poonamchouhan076.workers.dev/";
 const url8 = "https://primcricl-mud-e2d1.poonamchouhan076.workers.dev/";
     const url9 = "https://raw.githubusercontent.com/poonamchouhan54/Zee-channel-/refs/heads/main/output/zee_channels.m3u";
 
